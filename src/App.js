b4938c7706647504adc5c940d1572d03aba61396
realbenben <b.patou59@gmail.com>
2021-11-24 14:27:15 +0100

    add promise

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p> hello</p>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+//
+//
+//promesse dans des promesses (pas ouf😒)
+//
+// fetch("https://jsonplaceholder.typicode.com/todos/1").then((response) => {
+//   response = response.json();
+//   response.then((result) => {
+//     console.log(result);
+//   });
+// });
+//promesse faites a la suite (nice😜)
+//
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then((response) => {
+    return response.json();
+  })
+  .then((result) => {
+    console.log(result);
+  });
+//
+//
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Notre premier article:</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+fetch("https://jsonplaceholder.typicode.com/todos/1");
