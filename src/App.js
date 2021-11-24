e3a8c35edb7d263fb7895176f57320d941d4c436
realbenben <b.patou59@gmail.com>
2021-11-24 14:45:47 +0100

    screening the promise

--- src/App.js
+++ src/App.js
@@ -10,26 +10,40 @@ import React, { Component } from "react";
 //   });
 // });
 //promesse faites a la suite (nice😜)
-//
-fetch("https://jsonplaceholder.typicode.com/todos/1")
-  .then((response) => {
-    return response.json();
-  })
-  .then((result) => {
-    console.log(result);
-  });
+// //
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((result) => {
+//     console.log(result);
+//   });
 //
 //
 class App extends Component {
+  state = {
+    post: {},
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => {
+        return response.json();
+      })
+      .then((result) => {
+        // set time out pour simuler chargement(1500 = 1500ms/=1.5 seconde)👍🏻
+        setTimeout(() => {}, 1500);
+        this.setState({ post: result });
+      });
+  }
   render() {
     return (
       <div>
         <h1>Notre premier article:</h1>
+        <h2>{this.state.post.title}</h2>
       </div>
     );
   }
 }
 
 export default App;
-
-fetch("https://jsonplaceholder.typicode.com/todos/1");
